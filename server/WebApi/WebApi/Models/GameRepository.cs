d01f3b54d11ef4a4beb776a3ba69d5d5fd7673fd
Lmendien <chrisjfoss@gmail.com>
2017-04-12 11:41:56 -0400

    Set up asp net core backend and edited front end to try to get it to work. There are cross domain issues.

---
+++ server/WebApi/WebApi/Models/GameRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Yahtzee.Models
+{
+    public class GameRepository : IGameRepository
+    {
+        private readonly YahtzeeContext _context;
+
+        public GameRepository(YahtzeeContext context)
+        {
+            _context = context;
+
+            if (_context.Games.Count() == 0)
+            {
+                Add(new Game { User = "Test" });
+            }
+        }
+
+        public IEnumerable<Game> GetAll()
+        {
+            return _context.Games.ToList();
+        }
+
+        public void Add(Game game)
+        {
+            _context.Games.Add(game);
+            _context.SaveChanges();
+        }
+
+        public Game Find(long id)
+        {
+            var game = _context.Games.FirstOrDefault<Game>(t => t.Id == id);
+            return game;
+        }
+
+        public void Remove(long id)
+        {
+            var entity = _context.Games.FirstOrDefault<Game>(t => t.Id == id);
+            _context.Games.Remove(entity);
+            _context.SaveChanges();
+        }
+
+        public void Update(Game game)
+        {
+            _context.Games.Update(game);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ server/WebApi/WebApi/Models/YahtzeeContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Yahtzee.Models
+{
+    public class YahtzeeContext : DbContext
+    {
+        public YahtzeeContext(DbContextOptions<YahtzeeContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Game> Games { get; set; }
+    }
+}
